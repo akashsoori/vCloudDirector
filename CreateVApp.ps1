3eeb738c8d98508b7669b332dc48fddf0487bf4a
Akash Soori <akashsoori@gmail.com>
2022-07-03 13:09:12 +0530

    Create CreateVApp.ps1

---
+++ CreateVApp.ps1
@@ -0,0 +1,27 @@
+$ErrorActionPreference = "Stop"
+
+. C:\apps\VMWare\CloudOps\Library.ps1
+
+SelectedCloud
+    
+Login
+
+CloneVApp
+
+StopVApp
+
+ShareVApp
+
+UpdateCPUandRAM
+
+UpdateVMName
+
+StartVApp
+
+FetchExternalIP
+
+RetainExternalIP
+
+GetVMHostData
+
+Logout
