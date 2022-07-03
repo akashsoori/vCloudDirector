ce49f81a8d40e70189588f6f015e1ab7812a7817
Akash Soori <akashsoori@gmail.com>
2022-07-03 13:31:37 +0530

    Create CaptureVApp.ps1

---
+++ CaptureVApp.ps1
@@ -0,0 +1,17 @@
+$ErrorActionPreference = "Stop"
+
+. C:\apps\CloudOps\Library.ps1
+
+Login
+
+StopVApp
+
+UpdateCPUandRAM
+
+CaptureUpdateVAppMetadata
+
+CheckTemplateExists
+
+CaptureAsTemplate
+
+Logout
